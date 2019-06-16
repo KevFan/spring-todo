5fe9700768d056b4abc703177c16d239848621e5
Kevin Fan <kevin.fan@errigal.com>
2019-06-16 12:38:09 +0100

    fix test affecting persisting db

--- src/test/java/com/todo/base/IntegrationTestBase.java
+++ src/test/java/com/todo/base/IntegrationTestBase.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -13,7 +13,7 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @RunWith(SpringRunner.class)
-@PropertySource("classpath:test.properties")
+@TestPropertySource("classpath:test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class IntegrationTestBase {
     @Autowired
